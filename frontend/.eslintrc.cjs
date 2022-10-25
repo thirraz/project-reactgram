3d30e9f8bfa4117a116b717efd7d04d2452a02f2
Thiago Souza <thiagofernandosouza20@gmail.com>
2022-10-25 10:44:48 -0300

    eslint and prettier added

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended', "prettier"],
+    overrides: [],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+      'no-unused-vars': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+  }
