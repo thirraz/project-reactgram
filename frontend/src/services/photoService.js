40f242634e18ba52263cd72ad95af06851865370
Thiago Souza <thiagofernandosouza20@gmail.com>
2022-10-29 13:26:16 -0300

    photo posts functionality

---
+++ frontend/src/services/photoService.js
@@ -0,0 +1,20 @@
+import { api, requestConfig } from "../utils/config"
+
+//Publish an user photo
+export const publishPhoto = async (data, token) => {
+    const config = requestConfig("POST", data, token, true)
+
+    try {
+        const res = await fetch(api + "/photos", config)
+            .then((res) => res.json())
+            .catch((err) => err)
+
+        return res
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const photoService = {}
+
+export default photoService
